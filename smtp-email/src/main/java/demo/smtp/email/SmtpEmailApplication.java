1873d99d74c95e0da7e416d93107cc070a2c7aba
Inder <saininderpreet994@gmal.com>
2020-09-09 13:28:58 +0530

    first commit

---
+++ smtp-email/src/main/java/demo/smtp/email/SmtpEmailApplication.java
@@ -0,0 +1,13 @@
+package demo.smtp.email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmtpEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmtpEmailApplication.class, args);
+	}
+
+}
---
+++ smtp-email/src/test/java/demo/smtp/email/SmtpEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.smtp.email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmtpEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
